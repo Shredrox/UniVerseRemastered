8a78439c55f8cdfb591bfbca9f8351a68238e47f
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-18 21:14:33 +0200

    Create NewsCard component

---
+++ UniVerseUI/src/components/news/NewsCard.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from 'react-router-dom'
+import PinnedIcon from '../assets/icons/icon-pinned.svg'
+import useNewsImage from '../../hooks/query/useNewsImage';
+import NewsInterface from '../../interfaces/NewsInterface';
+
+interface NewsCardProps{
+  news: NewsInterface;
+}
+
+const NewsCard = ({news} : NewsCardProps) => {
+  const navigate = useNavigate();
+
+  const { newsImage } = useNewsImage(news.id);
+
+  return (
+    <div onClick={() => navigate(`/news/${news.id}`)} className={`news-card ${news.pinned ? 'news-card-pinned' : ''}`}>
+      <div className='news-image-container'>
+      {newsImage?.size > 0 && 
+      <img src={URL.createObjectURL(newsImage)} alt='NewsImage'/> 
+      }
+      </div>
+      <div className="news-content-container">
+        <h3 className="news-text">{news.title}</h3> 
+        <label>{news.date}</label>
+      </div>
+      <div>
+        {news.pinned && <img src={PinnedIcon} alt="PinnedIcon" />}
+      </div>
+    </div>
+  )
+}
+
+export default NewsCard
