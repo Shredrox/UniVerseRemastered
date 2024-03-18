ac454a3f6f8967d52aff1360d708a6ad16b5eb21
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-18 20:58:19 +0200

    Create newsService

---
+++ UniVerseUI/src/services/newsService.ts
@@ -0,0 +1,28 @@
+import axios from "../axios/axios";
+
+export const getNews = async () =>{
+  const response = await axios.get('/news');
+  return response.data;
+}
+
+export const getNewsImage = async (id : number) =>{
+  const response = await axios.get(`/news/${id}/image`, { responseType: 'blob'});
+  return response.data;
+}
+
+export const getNewsById = async (newsId : number) =>{
+  const response = await axios.get(`/news/${newsId}`);
+  return response.data;
+}
+
+export const addNews = async (news) =>{
+  return await axios.post('/news/createNews', news);
+}
+
+export const updateNews = async (data) =>{
+  return await axios.post(`/news/update`, data);
+}
+
+export const deleteNews = async (newsId : number) =>{
+  return await axios.delete(`/news/delete/${newsId}`, newsId);
+}
