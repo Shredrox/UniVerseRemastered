78fd4b77703af555b8856b5e38ebaa8180e6ca64
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-18 20:53:38 +0200

    Create ProtectedRoute

---
+++ UniVerseUI/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom"
+import useAuth from "../hooks/auth/useAuth";
+
+const ProtectedRoute = () => {
+  const { auth } = useAuth();
+  const location = useLocation();
+
+  return (
+    auth?.accessToken ? <Outlet/> : <Navigate to='/' state={{from: location}} replace />
+  )
+}
+
+export default ProtectedRoute
