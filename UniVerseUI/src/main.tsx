3fc0741a39228e7b2c68456cc6b70868f93bfcc8
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-18 20:30:41 +0200

    Update main.tsx

--- UniVerseUI/src/main.tsx
+++ UniVerseUI/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { AuthProvider } from './contexts/AuthContext.jsx'
+import './styles/style.css'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorPage from './pages/ErrorPage.jsx'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ErrorBoundary FallbackComponent={ErrorPage}>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false}/>
+        </AuthProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
+  </React.StrictMode>
 )
