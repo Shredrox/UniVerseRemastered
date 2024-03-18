94b8bb9a70f1296b36aec12cb205827d92747d85
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-18 20:26:38 +0200

    Create AuthContext

---
+++ UniVerseUI/src/contexts/AuthContext.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, createContext, useState } from "react"
+import AuthContextType from "../interfaces/auth/AuthContextType";
+import Auth from "../interfaces/auth/Auth";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({children} : AuthProviderProps) => {
+  const [auth, setAuth] = useState<Auth>({});
+
+  const contextValue : AuthContextType = {
+    auth,
+    setAuth
+  }
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext
