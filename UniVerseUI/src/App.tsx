93013461dd36e043e058009d83aca9f083d46300
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-18 21:30:39 +0200

    Update App.tsx

--- UniVerseUI/src/App.tsx
+++ UniVerseUI/src/App.tsx
@@ -1,10 +1,54 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import ProtectedRoute from './routes/ProtectedRoute'
+import Home from "./pages/Home"
+import News from './pages/News'
+import Jobs from './pages/Jobs'
+import Events from './pages/Events'
+import Settings from './pages/Settings'
+import Landing from './pages/Landing'
+import MainLayout from './layouts/MainLayout'
+import NewsDetails from './pages/NewsDetails'
+import Profile from './pages/Profile'
+import JobDetails from './pages/JobDetails'
+import Chat from './pages/Chat'
+import ChatsLayout from './layouts/ChatsLayout'
+import ErrorPage from './pages/ErrorPage'
+import NotFound from './pages/NotFound'
+import PersistLogin from './routes/PersistLogin'
+import RouterErrorFallback from './components/fallback/RouterErrorFallback'
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route errorElement={<ErrorPage/>}>
+        <Route path="/" element={<Landing/>}/>
+        <Route element={<MainLayout/>}>
+          <Route element={<PersistLogin/>}>
+            <Route element={<ProtectedRoute/>} errorElement={<RouterErrorFallback/>}>
+              <Route path="/home" element={<Home/>}/>
+              <Route path="/profile/:username" element={<Profile/>}/>
+              <Route path="/news" element={<News/>}/>
+              <Route path="/news/:newsId" element={<NewsDetails/>}/>
+              <Route path="/jobs" element={<Jobs/>}/>
+              <Route path="/jobs/:jobId" element={<JobDetails/>}/>
+              <Route path="/events" element={<Events/>}/>
+              <Route path="/chats" element={<ChatsLayout/>}>
+                <Route path="/chats" element={<div className='chat-not-selected'>Select chat</div>}/>
+                <Route path="/chats/:username" element={<Chat/>}/>
+              </Route>
+              <Route path="/settings" element={<Settings/>}/>
+            </Route>
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFound/>} />
+      </Route>
+    )
+  );
 
   return (
-    <>
-      
-    </>
+    <div className='app-container'>
+      <RouterProvider router={router}/>
+    </div>
   )
 }
 
