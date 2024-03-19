16c761e2fa7b87cdd96ffe849b86b57ba592e2ca
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-19 09:40:21 +0200

    Create Infrastructure ServiceExtensions

---
+++ UniVerseBackend/UniVerse.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UniVerse.Core.Entities;
+using UniVerse.Infrastructure.Data;
+
+namespace UniVerse.Infrastructure;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.AddDbContext<UniVerseDbContext>(options => 
+            options.UseNpgsql(configuration.GetConnectionString("UniVerseDb")));
+        
+        services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
+        services.AddAuthorizationBuilder();
+
+        services.AddIdentityCore<User>()
+            .AddEntityFrameworkStores<UniVerseDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
+}
