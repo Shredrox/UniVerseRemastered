e14c30b8e2e38219641c1136990b8d9aab63714b
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-19 09:40:03 +0200

    Create UniVerseDbContext

---
+++ UniVerseBackend/UniVerse.Infrastructure/Data/UniVerseDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UniVerse.Core.Entities;
+
+namespace UniVerse.Infrastructure.Data;
+
+public class UniVerseDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
+{
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Like> Likes { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Friendship> Friendships { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<News> News { get; set; }
+    public DbSet<JobOffer> JobOffers { get; set; }
+    public DbSet<GroupEvent> GroupEvents { get; set; }
+    public DbSet<Chat> Chats { get; set; }
+    public DbSet<Message> Messages { get; set; }
+}
