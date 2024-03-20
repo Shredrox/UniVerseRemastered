fdffa7d9b8e7137afdab6dfb1800f857d0617a8d
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 12:48:13 +0200

    Update UserController.cs

--- UniVerseBackend/UniVerse.Api/Controllers/UserController.cs
+++ UniVerseBackend/UniVerse.Api/Controllers/UserController.cs
@@ -3,57 +3,56 @@ using Microsoft.AspNetCore.Mvc;
 using UniVerse.Core.DTOs.Requests;
 using UniVerse.Core.Interfaces.IServices;
 
-namespace UniVerseBackend.Controllers
+namespace UniVerseBackend.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(
+    IUserService userService,
+    IAuthService authService) : ControllerBase
 {
-    [Authorize]
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController(
-        IUserService userService,
-        IAuthService authService) : ControllerBase
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
     {
-        [HttpGet]
-        public async Task<IActionResult> GetAllUsers()
-        {
-            return Ok(await userService.GetUsers());
-        }
+        return Ok(await userService.GetUsers());
+    }
         
-        [HttpGet("search/{filter}")]
-        public async Task<IActionResult> GetUsersByFilter(string filter)
-        {
-            return Ok(await userService.GetUsersByFilter(filter));
-        }
+    [HttpGet("search/{filter}")]
+    public async Task<IActionResult> GetUsersByFilter(string filter)
+    {
+        return Ok(await userService.GetUsersByFilter(filter));
+    }
         
-        [HttpGet("exists/{username}")]
-        public async Task<IActionResult> UserExists(string username)
-        {
-            return Ok(await userService.ExistsByUsername(username));
-        }
+    [HttpGet("exists/{username}")]
+    public async Task<IActionResult> UserExists(string username)
+    {
+        return Ok(await userService.ExistsByUsername(username));
+    }
         
-        [HttpGet("{username}/profile-picture")]
-        public async Task<IActionResult> GetUserProfilePicture(string username)
-        {
-            var imageBytes = await userService.GetUserProfilePicture(username);
+    [HttpGet("{username}/profile-picture")]
+    public async Task<IActionResult> GetUserProfilePicture(string username)
+    {
+        var imageBytes = await userService.GetUserProfilePicture(username);
 
-            if (imageBytes is null)
-            {
-                return NotFound();
-            }
+        if (imageBytes is null)
+        {
+            return NotFound();
+        }
             
-            return File(imageBytes, "image/jpeg");
-        }
+        return File(imageBytes, "image/jpeg");
+    }
         
-        [HttpPost("update-profile")]
-        public async Task<IActionResult> UpdateUserProfile([FromBody] UpdateProfileRequestDto request)
+    [HttpPost("update-profile")]
+    public async Task<IActionResult> UpdateUserProfile([FromBody] UpdateProfileRequestDto request)
+    {
+        var result = await userService.UpdateUserProfile(request);
+
+        if (!result)
         {
-            var result = await userService.UpdateUserProfile(request);
-
-            if (!result)
-            {
-                return BadRequest();
-            }
-
-            return Ok("Profile updated.");
+            return BadRequest();
         }
+
+        return Ok("Profile updated.");
     }
 }
