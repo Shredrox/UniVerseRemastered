0ae228bb224762af91ea7da2802ae3a9a55632dc
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 12:45:47 +0200

    Create FriendshipRepository

---
+++ UniVerseBackend/UniVerse.Core/Interfaces/IRepositories/IFriendshipRepository.cs
@@ -0,0 +1,16 @@
+﻿using UniVerse.Core.Entities;
+using UniVerse.Core.Enums;
+
+namespace UniVerse.Core.Interfaces.IRepositories;
+
+public interface IFriendshipRepository
+{
+    Task<Friendship?> GetFriendshipById(int friendshipId);
+    Task<Friendship?> GetFriendshipByUsers(User user1, User user2);
+    Task<List<Friendship>> GetFriendshipsByUserAndStatus(User user, FriendshipStatus status);
+    Task<List<Friendship>> GetFriendshipsByUser1OrUser2(User user1, User user2);
+    Task InsertFriendship(Friendship friendship);
+    Task UpdateFriendship(Friendship friendship);
+    Task DeleteFriendship(int friendshipId);
+    Task DeleteFriendshipByUsers(User user1, User user2);
+}
--- UniVerseBackend/UniVerse.Infrastructure/Repositories/FriendshipRepository.cs
+++ UniVerseBackend/UniVerse.Infrastructure/Repositories/FriendshipRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using UniVerse.Core.Entities;
 using UniVerse.Core.Enums;
+using UniVerse.Core.Exceptions;
 using UniVerse.Core.Interfaces.IRepositories;
 using UniVerse.Infrastructure.Data;
 
@@ -8,9 +9,16 @@ namespace UniVerse.Infrastructure.Repositories;
 
 public class FriendshipRepository(UniVerseDbContext context) : IFriendshipRepository
 {
-    public Task<Friendship> GetFriendshipByUsers(User user1, User user2)
+    public async Task<Friendship?> GetFriendshipById(int friendshipId)
     {
-        throw new NotImplementedException();
+        return await context.Friendships.FindAsync(friendshipId);
+    }
+
+    public async Task<Friendship?> GetFriendshipByUsers(User user1, User user2)
+    {
+        return await context.Friendships
+            .FirstOrDefaultAsync(f => (f.User1 == user1 && f.User2 == user2) || (f.User1 == user2 && f.User2 == user1));
+    
     }
 
     public async Task<List<Friendship>> GetFriendshipsByUserAndStatus(User user, FriendshipStatus status)
@@ -27,8 +35,42 @@ public class FriendshipRepository(UniVerseDbContext context) : IFriendshipReposi
             .ToListAsync();
     }
 
-    public Task DeleteFriendshipByUsers(User user1, User user2)
+    public async Task InsertFriendship(Friendship friendship)
     {
-        throw new NotImplementedException();
+        context.Friendships.Add(friendship);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task UpdateFriendship(Friendship friendship)
+    {
+        context.Entry(friendship).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteFriendship(int friendshipId)
+    {
+        var friendship = await context.Friendships.FindAsync(friendshipId);
+
+        if (friendship is null)
+        {
+            throw new NotFoundException();
+        }
+        
+        context.Friendships.Remove(friendship);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteFriendshipByUsers(User user1, User user2)
+    {
+        var friendship = await context.Friendships
+            .FirstOrDefaultAsync(f => (f.User1 == user1 && f.User2 == user2) || (f.User1 == user2 && f.User2 == user1));
+        
+        if (friendship is null)
+        {
+            throw new NotFoundException();
+        }
+        
+        context.Friendships.Remove(friendship);
+        await context.SaveChangesAsync();
     }
 }
