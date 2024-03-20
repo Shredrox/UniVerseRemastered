8b04d43aa89ba1288414a25319ae3755a30831d9
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 10:17:07 +0200

    Update User.cs

--- UniVerseBackend/UniVerse.Core/Entities/User.cs
+++ UniVerseBackend/UniVerse.Core/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
+using UniVerse.Core.Enums;
 
 namespace UniVerse.Core.Entities;
 
@@ -8,4 +9,8 @@ public class User : IdentityUser
     [MaxLength(256)]
     public string? RefreshToken { get; set; }
     public DateTime? RefreshTokenValidity { get; set; }
+    public UserRole Role { get; set; }
+    public byte[] ProfilePicture { get; set; }
+    public bool IsOnline { get; set; }
+    public bool IsEnabled { get; set; }
 }
