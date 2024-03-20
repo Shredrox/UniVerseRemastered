ac775a6cf78a3a810a9be155e77c22125f2123df
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 12:45:36 +0200

    Create NotFoundException

---
+++ UniVerseBackend/UniVerse.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace UniVerse.Core.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException()
+    {
+    }
+
+    public NotFoundException(string message) 
+        : base(message)
+    {
+    }
+    
+    public NotFoundException(string message, Exception inner) 
+        : base(message, inner)
+    {
+    }
+}
