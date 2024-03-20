bdbd34590466b18eb38f60338cfa2e7eb4abc5c5
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 12:46:12 +0200

    Create UserService

---
+++ UniVerseBackend/UniVerse.Core/Interfaces/IServices/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using UniVerse.Core.DTOs.Requests;
+using UniVerse.Core.DTOs.Responses;
+using UniVerse.Core.Entities;
+
+namespace UniVerse.Core.Interfaces.IServices;
+
+public interface IUserService
+{
+    Task<List<User>> GetUsers();
+    Task<List<UserResponseDto>> GetUsersByFilter(string filter);
+    Task<User?> GetUserById(string id);
+    Task<User?> GetUserByName(string name);
+    Task<User?> GetUserFromRefreshToken(string refreshToken);
+    Task<byte[]?> GetUserProfilePicture(string username);
+    Task<bool> ExistsByUsername(string username);
+    Task UpdateUserRefreshToken(User user);
+    Task<bool> UpdateUserProfile(UpdateProfileRequestDto request);
+}
---
+++ UniVerseBackend/UniVerse.Core/Services/UserService.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UniVerse.Core.DTOs.Requests;
+using UniVerse.Core.DTOs.Responses;
+using UniVerse.Core.Entities;
+using UniVerse.Core.Interfaces.IRepositories;
+using UniVerse.Core.Interfaces.IServices;
+
+namespace UniVerse.Core.Services;
+
+public class UserService(
+    IUserRepository userRepository,
+    UserManager<User> userManager) : IUserService
+{
+    public async Task<List<User>> GetUsers()
+    {
+        return await userRepository.GetUsers();
+    }
+
+    public async Task<List<UserResponseDto>> GetUsersByFilter(string filter)
+    {
+        var users = await userRepository.GetUsersByUsername(filter);
+
+        var response = users
+            .Select(u => new UserResponseDto(u.UserName, u.Email))
+            .ToList();
+
+        return response;
+    }
+
+    public async Task<User?> GetUserById(string id)
+    {
+        return await userRepository.GetUserById(id);
+    }
+        
+    public async Task<User?> GetUserByName(string name)
+    {
+        return await userRepository.GetUserByUsername(name);
+    }
+
+    public async Task<User?> GetUserFromRefreshToken(string refreshToken)
+    {
+        var user = await userRepository.GetUserByRefreshToken(refreshToken);
+
+        if (user is null)
+        {
+            return null;
+        }
+        
+        return await userRepository.GetUserById(user.Id);
+    }
+
+    public async Task<byte[]?> GetUserProfilePicture(string username)
+    {
+        var user = await userRepository.GetUserByUsername(username);
+        return user?.ProfilePicture;
+    }
+
+    public async Task<bool> ExistsByUsername(string username)
+    {
+        return await userRepository.ExistsByUsername(username);
+    }
+
+    public async Task UpdateUserRefreshToken(User user)
+    {
+        var existingUser = await userRepository.GetUserById(user.Id);
+
+        if(existingUser == null)
+        {
+            return;
+        }
+
+        existingUser.RefreshToken = user.RefreshToken;
+        existingUser.RefreshTokenValidity = user.RefreshTokenValidity;
+
+        await userRepository.UpdateUser(user);
+    }
+
+    public async Task<bool> UpdateUserProfile(UpdateProfileRequestDto request)
+    {
+        var user = await userRepository.GetUserByUsername(request.Username);
+
+        if (user == null)
+        {
+            return false;
+        }
+
+        if (!string.IsNullOrEmpty(request.NewUsername))
+        {
+            user.UserName = request.NewUsername;
+        }
+        if (!string.IsNullOrEmpty(request.NewEmail))
+        {
+            user.Email = request.NewEmail;
+        }
+        if (!string.IsNullOrEmpty(request.NewPassword))
+        {
+            user.PasswordHash = userManager.PasswordHasher.HashPassword(user, request.NewPassword);
+        }
+        if (request.ProfilePicture != null)
+        {
+            using var memoryStream = new MemoryStream();
+            await request.ProfilePicture.CopyToAsync(memoryStream);
+            user.ProfilePicture = memoryStream.ToArray();
+        }
+
+        await userRepository.UpdateUser(user);
+
+        return true;
+    }
+}
