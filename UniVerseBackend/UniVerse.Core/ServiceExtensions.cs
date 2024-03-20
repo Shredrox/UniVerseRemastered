1d3cd5f437f27b5e2eee2e4408b8fb3e7879b956
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 12:46:47 +0200

    Create core ServiceExtensions

---
+++ UniVerseBackend/UniVerse.Core/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UniVerse.Core.Interfaces.IServices;
+using UniVerse.Core.Services;
+
+namespace UniVerse.Core;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddCoreServices(this IServiceCollection services)
+    {
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IFriendshipService, FriendshipService>();
+        services.AddScoped<IPostService, PostService>();
+        
+        return services;
+    }
+}
