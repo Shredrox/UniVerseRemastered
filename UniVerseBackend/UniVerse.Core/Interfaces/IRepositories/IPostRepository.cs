d8a492ccd8f479a70553f770045e41b126e9d6db
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 12:15:35 +0200

    Create PostRepository

---
+++ UniVerseBackend/UniVerse.Core/Interfaces/IRepositories/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using UniVerse.Core.Entities;
+
+namespace UniVerse.Core.Interfaces.IRepositories;
+
+public interface IPostRepository
+{
+    Task<Post?> GetPostById(int id);
+    Task<List<Post>> GetPosts();
+    Task<List<Post>> GetPostsByAuthorNames(List<string> usernames);
+    Task<List<Post>> GetPostsByAuthorName(string authorName);
+    Task InsertPost(Post post);
+    Task UpdatePost(Post post);
+    Task DeletePost(int postId);
+}
---
+++ UniVerseBackend/UniVerse.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniVerse.Core.Entities;
+using UniVerse.Core.Interfaces.IRepositories;
+using UniVerse.Infrastructure.Data;
+
+namespace UniVerse.Infrastructure.Repositories;
+
+public class PostRepository(UniVerseDbContext context) : IPostRepository
+{
+    public async Task<Post?> GetPostById(int id)
+    {
+        return await context.Posts.FindAsync(id);
+    }
+
+    public async Task<List<Post>> GetPosts()
+    {
+        return await context.Posts.ToListAsync();
+    }
+
+    public async Task<List<Post>> GetPostsByAuthorNames(List<string> usernames)
+    {
+        return await context.Posts
+            .Where(p => usernames.Contains(p.AuthorName))
+            .ToListAsync();
+    }
+
+    public async Task<List<Post>> GetPostsByAuthorName(string authorName)
+    {
+        return await context.Posts
+            .Where(p => p.AuthorName == authorName)
+            .ToListAsync();
+    }
+
+    public async Task InsertPost(Post post)
+    {
+        context.Posts.Add(post);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task UpdatePost(Post post)
+    {
+        context.Entry(post).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeletePost(int postId)
+    {
+        var post = await context.Posts.FindAsync(postId);
+        context.Posts.Remove(post);
+        await context.SaveChangesAsync();
+    }
+}
