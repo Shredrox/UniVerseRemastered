55617f6183fa893dd394be0c885fab5ebef32f88
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 12:46:02 +0200

    Create TokenService

---
+++ UniVerseBackend/UniVerse.Core/Interfaces/IServices/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using UniVerse.Core.Entities;
+
+namespace UniVerse.Core.Interfaces.IServices;
+
+public interface ITokenService
+{
+    string CreateAccessToken(User user);
+    Task<string> CreateRefreshToken(User user);
+}
---
+++ UniVerseBackend/UniVerse.Core/Services/TokenService.cs
@@ -0,0 +1,49 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using UniVerse.Core.Entities;
+using UniVerse.Core.Interfaces.IRepositories;
+using UniVerse.Core.Interfaces.IServices;
+
+namespace UniVerse.Core.Services;
+
+public class TokenService(
+    IConfiguration configuration,
+    IUserRepository userRepository) : ITokenService
+{
+    public string CreateAccessToken(User user)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Email, user.Email)
+        };
+        
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            configuration.GetSection("Jwt:Key").Value!));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+        
+        var token = new JwtSecurityToken(
+            claims: claims, 
+            expires: DateTime.Now.AddHours(1),
+            signingCredentials: credentials);
+        
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+        return jwt;
+    }
+    
+    public async Task<string> CreateRefreshToken(User user)
+    {
+        var refreshToken = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+        
+        user.RefreshToken = refreshToken;
+        user.RefreshTokenValidity = DateTime.Now.AddHours(2).ToUniversalTime(); 
+        
+        await userRepository.UpdateUser(user);
+        
+        return refreshToken;
+    }
+}
