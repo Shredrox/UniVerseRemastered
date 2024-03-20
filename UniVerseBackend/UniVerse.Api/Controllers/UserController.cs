2ade2e2bf09bc429bf420ca8ac9d7ad17e8f5173
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 13:51:35 +0200

    Update UserController.cs

--- UniVerseBackend/UniVerse.Api/Controllers/UserController.cs
+++ UniVerseBackend/UniVerse.Api/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UniVerse.Core.DTOs.Requests;
+using UniVerse.Core.DTOs.Responses;
 using UniVerse.Core.Interfaces.IServices;
 
 namespace UniVerseBackend.Controllers;
@@ -28,6 +29,18 @@ public class UserController(
     public async Task<IActionResult> UserExists(string username)
     {
         return Ok(await userService.ExistsByUsername(username));
+    }
+    
+    [HttpGet("{username}")]
+    public async Task<IActionResult> GetUserByName(string username)
+    {
+        var user = await userService.GetUserByName(username);
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(new UserResponseDto(user.UserName, user.Email));
     }
         
     [HttpGet("{username}/profile-picture")]
