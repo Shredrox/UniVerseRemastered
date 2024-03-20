79606ec768b8bd6b0b3b8297fb7f9b3b082c69d8
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 12:47:06 +0200

    Create CreatePostRequestDto.cs

---
+++ UniVerseBackend/UniVerse.Core/DTOs/Requests/CreatePostRequestDto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace UniVerse.Core.DTOs.Requests;
+
+public record CreatePostRequestDto(
+    string Title,
+    string Content,
+    string AuthorName,
+    IFormFile Image);
