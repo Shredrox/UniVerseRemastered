103d8123dbe490fcea725f15a4fa0fe3df5b12a3
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 17:25:54 +0200

    Update controllers

--- UniVerseBackend/UniVerse.Api/Controllers/CommentController.cs
+++ UniVerseBackend/UniVerse.Api/Controllers/CommentController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UniVerse.Core.DTOs.Requests;
 using UniVerse.Core.Interfaces.IServices;
 
 namespace UniVerseBackend.Controllers;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class CommentController(ICommentService commentService) : ControllerBase
--- UniVerseBackend/UniVerse.Api/Controllers/FriendshipController.cs
+++ UniVerseBackend/UniVerse.Api/Controllers/FriendshipController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UniVerse.Core.Interfaces.IServices;
 
 namespace UniVerseBackend.Controllers;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class FriendshipController(IFriendshipService friendshipService) : ControllerBase
