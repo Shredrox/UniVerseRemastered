029394aaf0342ac279bff339b45d61515e643286
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-20 12:16:09 +0200

    Update UserRepository

--- UniVerseBackend/UniVerse.Core/Interfaces/IRepositories/IUserRepository.cs
+++ UniVerseBackend/UniVerse.Core/Interfaces/IRepositories/IUserRepository.cs
@@ -4,9 +4,14 @@ namespace UniVerse.Core.Interfaces.IRepositories;
 
 public interface IUserRepository
 {
-    Task<User> GetUserByEmail(string email);
-    Task<User> GetUserByUsername(string username);
-    Task<IEnumerable<User>> GetUsersByUsername(string username);
-    Task<IEnumerable<User>> GetUsersByUsernames(List<string> usernames);
+    Task<User?> GetUserById(string id);
+    Task<List<User>> GetUsers();
+    Task<User?> GetUserByEmail(string email);
+    Task<User?> GetUserByUsername(string username);
+    Task<List<User>> GetUsersByUsername(string username);
+    Task<List<User>> GetUsersByUsernames(List<string> usernames);
+    Task<User?> GetUserByRefreshToken(string refreshToken);
     Task<bool> ExistsByUsername(string username);
+    Task InsertUser(User user, string password);
+    Task UpdateUser(User user);
 }
--- UniVerseBackend/UniVerse.Infrastructure/Repositories/UserRepository.cs
+++ UniVerseBackend/UniVerse.Infrastructure/Repositories/UserRepository.cs
@@ -1,6 +1,68 @@
-﻿namespace UniVerse.Infrastructure.Repositories;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UniVerse.Core.Entities;
+using UniVerse.Core.Interfaces.IRepositories;
+using UniVerse.Infrastructure.Data;
 
-public class UserRepository
+namespace UniVerse.Infrastructure.Repositories;
+
+public class UserRepository(
+    UserManager<User> userManager,
+    UniVerseDbContext context) : IUserRepository
 {
+    public async Task<User?> GetUserById(string id)
+    {
+        return await userManager.FindByIdAsync(id);
+    }
     
+    public async Task<List<User>> GetUsers()
+    {
+        return await context.Users.ToListAsync();
+    }
+    
+    public async Task<User?> GetUserByEmail(string email)
+    {
+        return await userManager.FindByEmailAsync(email);
+    }
+
+    public async Task<User?> GetUserByUsername(string username)
+    {
+        return await userManager.FindByNameAsync(username);
+    }
+
+    public async Task<List<User>> GetUsersByUsername(string username)
+    {
+        return await context.Users
+            .Where(u => u.UserName == username)
+            .ToListAsync();
+    }
+
+    public async Task<List<User>> GetUsersByUsernames(List<string> usernames)
+    {
+        return await context.Users
+            .Where(p => usernames.Contains(p.UserName))
+            .ToListAsync();
+    }
+    
+    public async Task<User?> GetUserByRefreshToken(string refreshToken)
+    {
+        return await context.Users
+            .FirstOrDefaultAsync(u => u.RefreshToken == refreshToken && u.RefreshTokenValidity > DateTime.Now.ToUniversalTime());
+    }
+
+    public Task<bool> ExistsByUsername(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task InsertUser(User user, string password)
+    {
+        user.PasswordHash = userManager.PasswordHasher.HashPassword(user, password);
+        await userManager.CreateAsync(user, password);
+    }
+
+    public async Task UpdateUser(User user)
+    {
+        await userManager.UpdateAsync(user);
+    }
 }
