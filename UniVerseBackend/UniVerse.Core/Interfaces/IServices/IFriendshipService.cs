bf58c60ee2cd5f081645ed97b703a509aa499180
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-21 13:12:17 +0200

    Update IFriendshipService.cs

--- UniVerseBackend/UniVerse.Core/Interfaces/IServices/IFriendshipService.cs
+++ UniVerseBackend/UniVerse.Core/Interfaces/IServices/IFriendshipService.cs
@@ -1,4 +1,5 @@
-﻿using UniVerse.Core.DTOs.Responses;
+﻿using UniVerse.Core.DTOs.Requests;
+using UniVerse.Core.DTOs.Responses;
 using UniVerse.Core.Entities;
 using UniVerse.Core.Enums;
 
@@ -10,7 +11,7 @@ public interface IFriendshipService
     Task<List<string>> GetFriendsUsernames(string username);
     Task<List<User>> GetOnlineFriends(string username);
     Task<FriendshipStatus> CheckUsersFriendship(string user1Name, string user2Name);
-    Task CreateFriendship(string senderName, string receiverName);
+    Task<FriendRequestResponseDto> CreateFriendship(FriendRequestDto request);
     Task AcceptFriendRequest(int friendshipId);
     Task RejectFriendRequest(int friendshipId);
     Task DeleteFriendship(string user1Name, string user2Name);
