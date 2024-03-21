1cec7972326eb3d12c6d14cde8f0654081744c04
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-21 13:15:32 +0200

    Create SocketContextType interface

---
+++ UniVerseFrontend/src/interfaces/SocketContextType.ts
@@ -0,0 +1,23 @@
+import ChatInterface from "./ChatInterface";
+import FriendRequest from "./FriendRequest";
+import Message from "./Message";
+import Notification from "./Notification";
+
+export default interface SocketContextType {
+  notifications: Notification[];
+  messages: Message[];
+  sendNotification: ({message, type, source, recipientName} : { message: string, type: string, source: string, recipientName : string }) => Promise<void>;
+  setUserNotifications: (notifications : Notification[]) => void;
+  setUserChats: (newUserChats : ChatInterface[]) => void;
+  createHubConnection: () => void;
+  disconnectFromHub: () => void;
+  setChatMessages: (newMessages : Message[]) => void; //
+  sendMessage: ({ message, sender, receiver }: { message: string; sender: string; receiver: string; }) => Promise<void>;
+  createChat: (chat: ChatInterface) => Promise<void>;
+  chats: ChatInterface[];
+  friendRequests: FriendRequest[];
+  sendFriendRequest: (friendRequest : {sender : string, receiver : string}) => Promise<void>;
+  setUserFriendRequests: (newFriendRequests : FriendRequest[]) => void;
+  newOnlineFriend: boolean;
+  setNewOnlineFriend: React.Dispatch<React.SetStateAction<boolean>>;
+}
