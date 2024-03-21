ff2ef759249f0f1b6c2b25fc90442540cf22563f
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-21 14:26:23 +0200

    Update userService.ts

--- UniVerseFrontend/src/services/userService.ts
+++ UniVerseFrontend/src/services/userService.ts
@@ -5,42 +5,42 @@ export const getUsers = async () =>{
   return response.data;
 }
 
-export const getUsersByFilter = async (filter) =>{
+export const getUsersByFilter = async (filter : string) =>{
   const response = await axios.get(`User/search/${filter}`);
   return response.data;
 }
 
-export const userExists = async (username) =>{
+export const userExists = async (username : string) =>{
   const response = await axios.get(`User/exists/${username}`);
   return response.data;
 }
 
-export const getUserByName = async (username) =>{
+export const getUserByName = async (username : string) =>{
   const response = await axios.get(`User/${username}`);
   return response.data;
 }
 
-export const getUserOnlineFriends = async (username) =>{
+export const getUserOnlineFriends = async (username : string) : Promise<string[]> =>{
   const response = await axios.get(`Friendship/${username}/online-friends`);
   return response.data;
 }
 
-export const getUserFriendsCount = async (username) =>{
+export const getUserFriendsCount = async (username : string) =>{
   const response = await axios.get(`Friendship/${username}/count`);
   return response.data;
 }
 
-export const getUserProfilePicture = async (username) =>{
+export const getUserProfilePicture = async (username : string) =>{
   const response = await axios.get(`User/${username}/profile-picture`, { responseType: 'blob'});
   return response.data;
 }
 
-export const getUserFriendRequests = async (username) =>{
+export const getUserFriendRequests = async (username : string) =>{
   const response = await axios.get(`Friendship/${username}/friend-requests`);
   return response.data;
 }
 
-export const checkFriendship = async (user1Name, user2Name) => {
+export const checkFriendship = async (user1Name : string, user2Name : string) => {
   const response = await axios.get('Friendship/check-friendship', {
     params: {
       user1Name,
@@ -56,7 +56,7 @@ export const addFriend = async ({ loggedInUser, profileUser }) =>{
   return response.data;
 }
 
-export const acceptFriendRequest = async (friendshipId) =>{
+export const acceptFriendRequest = async (friendshipId : number) =>{
   const response = await axios.post(`Friendship/accept-friend-request/${friendshipId}`);
   return response.data;
 }
@@ -66,7 +66,7 @@ export const removeFriend = async ({ loggedInUser, profileUser }) =>{
   return response.data;
 }
 
-export const rejectFriendRequest = async (friendshipId) =>{
+export const rejectFriendRequest = async (friendshipId : number) =>{
   const response = await axios.delete(`Friendship/reject-friend-request/${friendshipId}`);
   return response.data;
 }
