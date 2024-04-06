9e32ecd6361ed7b976d149c516595629423ab75a
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-04-06 11:40:27 +0300

    Create Job interface

---
+++ UniVerseFrontend/src/interfaces/Job.ts
@@ -0,0 +1,10 @@
+export default interface Job{
+  id: number;
+  title: string;
+  company: string;
+  description: string;
+  requirements: string;
+  location: string;
+  type: string;
+  salary: string;
+}
